c852d619d54e70ffb7c583abd93e9e3e062b16fd
William Holcombe <sphinxelite@Williams-iMac.local>
2019-08-07 10:35:12 -0600

    hero commit

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 //**** */
 import Header from "./Header";
 import Meta from "./Meta";
+import styled from "styled-components";
+
+const Mybutton = styled.button`
+  background: red;
+  font-size: 100px;
+`;
 
 export default class Page extends Component {
   render() {
@@ -9,7 +15,8 @@ export default class Page extends Component {
       <div>
         <Meta />
         <Header />
-        <p>Hey I'm the page component</p>
+        <Mybutton>Click Me</Mybutton>
+        <p>Hey I'm the page component HERO 6</p>
         {this.props.children}
       </div>
     );
