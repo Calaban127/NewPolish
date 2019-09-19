0f85afbb2dec58d505f56395b877410e9ce561eb
William Holcombe <sphinxelite@Williams-iMac.local>
2019-09-18 19:24:02 -0600

    13

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+//  const Mutations = {
+// doesntBelong() {}
+//  };
+//  =
+// rror: Mutation.doesntBelong defined in resolvers, but not in schema
+//
+
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    //never do this 13-930
+    global.dogs = global.dogs || [];
+    //create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+    console.log(args);
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+//we're going to hook ours up to Prisma, but whatever you hook it up
+//to - local storage etc. - you're going to but your db calls
+//are going to go
+
+const Query = {
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
+
+// return [{ name: "Snickers" }, { name: "Sunny" }];
